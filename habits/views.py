feba2be4dbc5319417af6d1b1427bfaaf98e326d
hoolywant <sir.saltickow@yandex.ru>
2023-11-06 16:18:27 +0400

    pagination is done

--- habits/views.py
+++ habits/views.py
@@ -2,14 +2,17 @@ from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
 from habits.models import Habits
+from habits.pagination import MyPagination
 from habits.permissions import ViewSetPermission
 from habits.serializers import HabitsSerializer
 
 
 class HabitsViewSet(viewsets.ModelViewSet):
+    pagination_class = MyPagination
     serializer_class = HabitsSerializer
     queryset = Habits.objects.all()
     permission_classes = (ViewSetPermission, )
+
 
     def perform_create(self, serializer):
         new_habit = serializer.save()
@@ -18,6 +21,12 @@ class HabitsViewSet(viewsets.ModelViewSet):
         if not Habits.objects.get(pk=new_habit.linked_id).is_pleasant:
             raise "A related habit can only be pleasant"
 
+    def get(self, request):
+        queryset = Habits.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = HabitsSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
+
 
 
 class HabitsListAPI(generics.ListAPIView):
@@ -25,3 +34,17 @@ class HabitsListAPI(generics.ListAPIView):
     queryset = Habits.objects.all()
     permission_classes = [IsAuthenticated]
 
+class MyPagination(PageNumberPagination):
+    page_size = 10  # Количество элементов на странице
+    page_size_query_param = 'page_size'  # Параметр запроса для указания количества элементов на странице
+    max_page_size = 100  # Максимальное количество элементов на странице
+
+# views.py
+class MyView(APIView):
+    pagination_class = MyPagination
+
+    def get(self, request):
+        queryset = MyModel.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = MySerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
